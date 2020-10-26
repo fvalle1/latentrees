6584c708dab4670472a9499a224d1eb24c71ee42
Filippo Valle <filippo.valle@to.infn.it>
2020-10-26 16:48:59 +0100

    updates

--- latentrees/models.py
+++ latentrees/models.py
@@ -2,11 +2,12 @@ from latentrees.layers.layer import layer
 from latentrees.layers.first_layer import first_layer
                 
 class model():
-    def __init__(self, L=50, nl=3, distribution=None):
+    def __init__(self, L=50, nl=3, distribution=None, name="model"):
         self.L = L
         self.nl = nl
         self.levels = []
         self.distribution = distribution
+        self.name=name
         
     def run(self) -> None:
         self.levels = [first_layer("l0", self.nl, distribution=self.distribution)]
@@ -23,4 +24,4 @@ class model():
         return self.levels
     
     def __repr__(self):
-        return "model with {} layers and {} splits".format(self.L, self.nl)
+        return "{} with {} layers and {} splits".format(self.name,self.L, self.nl)
