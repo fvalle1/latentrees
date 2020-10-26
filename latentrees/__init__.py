e75222c93828c35137b8b83b243e4273a7e61c8f
Filippo Valle <filippo.valle@to.infn.it>
2020-10-26 15:27:41 +0100

    fix version

--- latentrees/__init__.py
+++ latentrees/__init__.py
@@ -3,3 +3,5 @@ from latentrees.layers.layer import *
 from latentrees.layers.first_layer import *
 from latentrees.models import *
 from latentrees.analyses import *
+
+__version__="0.0.0.1"
--- latentrees/layers/first_layer.py
+++ latentrees/layers/first_layer.py
@@ -4,6 +4,6 @@ class first_layer(layer):
     def sample(self, previous_layer):
         raise NotImplementedError("First layer cannot have a previous layer")
             
-    def sample(self) -> None:
+    def sample(self, first_mean = 1) -> None:
         for node in range(self.nl):
-            self.nodes.append(self.distribution(1))
+            self.nodes.append(self.distribution(first_mean))
