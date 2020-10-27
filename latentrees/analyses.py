122f04e17c74d3347e65ca1f926be9047125f033
Filippo Valle <filippo.valle@to.infn.it>
2020-10-27 11:31:20 +0100

    new plots on correlations

--- latentrees/analyses.py
+++ latentrees/analyses.py
@@ -3,12 +3,33 @@ from latentrees.models import model
 class analyses():
     def __init__(self):
         self.models = []
+        self.iterindex = -1
         
     def run(self) -> None:
+        import multiprocessing as mp
         def run_model(model):
-            print("Running ", model)
-            model.run()
+            try:
+                print("Running ", model)
+                model.run()
+                return True
+            except:
+                return False
+            
         list(map(run_model, self.models))
+        
+    def model_generator(self):
+        for model in self.models:
+            yield model
+        
+    def __iter__(self):
+        self.iterindex = -1
+        return self
+    
+    def __next__(self):
+        self.iterindex += 1
+        if self.iterindex < len(self.models):
+            return self.models[self.iterindex]
+        raise StopIteration
     
     def __getitem__(self, item):
         return self.models[item]
