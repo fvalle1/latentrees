bd4aa4dadca707c96c59b07aa8487a0d2095f029
Filippo Valle <filippo.valle@to.infn.it>
2020-10-29 09:52:49 +0100

    fix init

--- latentrees/__init__.py
+++ latentrees/__init__.py
@@ -1,7 +1,6 @@
 from latentrees.nodes import *
-from latentrees.layers.layer import *
-from latentrees.layers.first_layer import *
 from latentrees.models import *
 from latentrees.analyses import *
+import latentrees.layers
 
 __version__="0.0.0.1"
