d80da630c138ca4f2bbd5af72472d33a02447a40
Filippo Valle <filippo.valle@to.infn.it>
2020-10-29 09:48:22 +0100

    add tests

--- test.py
+++ test.py
@@ -9,6 +9,22 @@ class latenttests(unittest.TestCase):
 		import latentrees
 		self.assertEqual(latentrees.__version__[0],"0")
         
-        
+	def test_stop_iter(self):
+		from latentrees import analyses
+		runtime=analyses()
+		with self.assertRaises(StopIteration):
+			next(runtime) 
+
+	def test_model_load(self):
+		from latentrees import analyses
+		from latentrees import model
+		runtime=analyses()
+		runtime.append_model(model())
+		runtime.append_model(L=10)
+
+		self.assertIsInstance(runtime[0],model)
+		self.assertIsInstance(runtime[1],model)
+
+
 if __name__ == "__main__":
 	unittest.main()
