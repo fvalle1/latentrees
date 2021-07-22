aaf62c206ae069e4f4e6edb0b1d14994ccfbb0dc
Filippo Valle <filippo.valle@to.infn.it>
2021-07-22 10:37:36 +0200

    fix OpenCL on Linux

--- independentrees/cpp/main.cpp
+++ independentrees/cpp/main.cpp
@@ -15,22 +15,21 @@ using std::endl;
 using std::cout;
 
 typedef long double TData;
-typedef pcg64 pcg_t;
 
 // typedef std::uniform_int_distribution<TData> distribution;
-// typedef Generator<distribution, pcg_t, TData> rngenerator;
+// typedef Generator<distribution, pcg32_fast, TData> rngenerator;
 // typedef UniformModel<rngenerator, distribution, TData> model;
 
 // typedef std::normal_distribution<TData> distribution;
-// typedef Generator<distribution, pcg_t, TData> rngenerator;
+// typedef Generator<distribution, pcg32_fast, TData> rngenerator;
 // typedef GausModel<rngenerator, distribution, TData> model;
 
 // typedef std::negative_binomial_distribution<TData> distribution;
-// typedef Generator<distribution, pcg_t, TData> rngenerator;
+// typedef Generator<distribution, pcg32_fast, TData> rngenerator;
 // typedef NbinModel<rngenerator, distribution, TData> model;
 
 typedef std::gamma_distribution<TData> distribution;
-typedef Generator<distribution, pcg_t, TData> rngenerator;
+typedef Generator<distribution, pcg32_fast, TData> rngenerator;
 typedef GammaModel<rngenerator, distribution, TData> model;
 
 
